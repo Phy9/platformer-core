ea46e303ebf543284deed9cd840ee90126a6b5b7
Tongyu Lu <lutongyu2004@outlook.com>
2020-01-04 23:04:30 +0800

    Save work

---
+++ main.py
@@ -0,0 +1,48 @@
+# Integrated modules
+import sys, os
+
+# Installed modules
+import pygame
+
+# Local files
+import myutils
+
+# Set up Pygame window
+pygame.init()
+win = pygame.display.set_mode(myutils.consts.WN_RES)
+pygame.display.set_caption(myutils.consts.WN_TITLE)
+
+# Other setups
+
+# Mainloop
+
+running = 1
+clock = pygame.time.Clock()
+FPS = myutils.consts.WN_FPS
+
+while running:
+    clock.tick_busy_loop(FPS)
+    deltaTime = clock.get_time()
+    if running == 1:
+        deltaTime = 0
+        running = 2
+    elif running == 2:
+        WN_FPS = min(1 / (2 / myutils.consts.WN_FPS - deltaTime), 240)  # Hold FPS stable
+
+    # Events
+    events = pygame.event.get()
+    keyStrokes = pygame.key.get_pressed()
+
+    # Update
+    for e in events:
+        if e.type == pygame.QUIT:
+            running = False
+            break
+
+    # Draw
+    win.fill(myutils.colors.rgb("#fff"))
+    pygame.display.flip()
+
+# Quit
+
+pygame.quit()
---
+++ myutils/consts.py
@@ -0,0 +1,5 @@
+# Window settings
+
+WN_RES = (512, 288)
+WN_FPS = 60
+WN_TITLE = "Platformer Core"
